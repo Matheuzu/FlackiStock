8cf1fc80389b70b06afa00a1c2c4f5170ab7f316
Matheus Paiva <matheuzu@hotmail.com>
2020-05-17 21:43:58 -0300

    Adicionando "Nova Ação"
    Adicionei mais um botão, que irá adicionar mais ações além das existentes.

--- js/aparecer.js
+++ js/aparecer.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
-    $("#aa").click(function(){
+  $("#aa").click(function(){
+    $("#novo").slideToggle(800);
+    $("#novo").css("display", "inline-block");
+  });
+});
+
+$(document).ready(function(){
+    $("#bb").click(function(){
       $("#indicar").slideToggle(800);
       $("#indicar").css("display", "inline-block");
     });
   });
 
   $(document).ready(function(){
-    $("#bb").click(function(){
+    $("#cc").click(function(){
       $("#quantidade").slideToggle(800);
       $("#quantidade").css("display", "inline-block");
     });
